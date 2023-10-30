5d5724690ed1dea486885b358c1820cd4541cf11
ZZU1U <zzu1u@list.ru>
2023-10-30 22:45:43 +0300

    First commit

--- calcuating.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LinearRegression
-
-
-def train_model(X, Y):
-    model = LinearRegression()
-    model.fit(X, Y)
-
-    return model
--- plotting.py
+++ plotting.py
@@ -1,8 +1,21 @@
-import matplotlib as mpl
+import numpy as np
 from matplotlib import pyplot as plt
+from calculating import *
 
-def make_plot(X, Y, model):
+
+def make_plot(X: np.array, Y: np.array, model: LinearRegression | SimpleRegression | MeanValue,\
+              X_label: None | str = None, Y_label: None | str = None, filename: str = 'plot.png'):
+    # First cleaning our last plot
+    plt.cla()
+    plt.clf()
+
     plt.scatter(X, Y, color='b')
-    plt.plot(X, model.predict(X),color='r')
+    plt.plot(X, model.predict(X), color='r', )
 
-    plt.savefig('plot.png')
+    if X_label is not None:
+        plt.xlabel(X_label)
+    
+    if Y_label is not None:
+        plt.ylabel(Y_label)
+
+    plt.savefig(filename)
